09ecaae473015a9f534bf501e63cccf38012a4f1
Matthew Wilding <mbwilding@gmail.com>
2026-02-09 23:04:48 +0800

    fix(cicd)

--- crates/cli/build.rs
+++ crates/cli/build.rs
@@ -4,10 +4,16 @@ use std::path::PathBuf;
 fn main() {
     let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    let workspace_root = manifest_dir
+    let mut workspace_root = manifest_dir
         .parent()
         .and_then(|p| p.parent())
         .expect("Failed to find workspace root");
+
+    if std::env::var("CI").is_ok() {
+        workspace_root = workspace_root
+            .parent()
+            .expect("Failed to find CI workspace root");
+    }
 
     println!(
         "cargo:rustc-env=WORKSPACE_ROOT={}",
